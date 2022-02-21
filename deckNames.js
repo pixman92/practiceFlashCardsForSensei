c8e6db02f8df47be87780e606a7b12eead1d2d63
TheArborist <GOtrustD@verizon.net>
2022-02-20 18:32:17 -0800

    2022-02-20 - 10 minute Looping Commit

--- deckNames.js
+++ deckNames.js
@@ -13,61 +13,33 @@ function addToDeckNames(name){
     var alreadyThereBool = false;           // is the deck already named? this BOOL keeps track!
     try{
         // if(deckNamesJSON.JSONobj.innerArray == null || deckNamesJSON.JSONobj.innerArray == undefined){
-        // if(deckNamesJSON.JSONobj.innerArray == null || deckNamesJSON.JSONobj.innerArray == undefined){
+        if(deckNamesJSON.JSONobj.innerArray == null || deckNamesJSON.JSONobj.innerArray == undefined){
             deckNamesJSON.addToObj([[[0], [['deckName', name]]]]);
-            debugger;    //set to debug at this line    
-        // }
+            // debugger;    //set to debug at this line    
+        }
+        for(var i=0; i<deckNamesJSON.JSONobj.innerArray.length; i++){
+            // debugger;    //set to debug at this line
+            if(deckNamesJSON.JSONobj.innerArray[i][0][0][1]==name){
+                alreadyThereBool = true;
+                // debugger;    //set to debug at this line
+            }
+        }
     }catch(err){
         console.error('ERR ', err);
     }
-    if(deckNamesJSON.JSONobj.innerArray >= 1){
-               // deckNamesJSON.JSONobj.innerArray.forEach((item, index)=>{
-            // debugger;    //set to debug at this line
-        //     // if(item[0][1]!=name && index>=deckNamesJSON.JSONobj.innerArray.length){
-        //     if(item[0][1]==name){
-        //         debugger;    //set to debug at this line
-        //         console.log('Already a Name!');
-        //     }else if(index==deckNamesJSON.JSONobj.innerArray.length){
-        //         console.log('still within index');
-        //     }else{
-        //         debugger;    //set to debug at this line
-        //         deckNamesJSON.addToObj([[[0], ['deckName', name]]]);
-        //     }
-        //     // debugger;    //set to debug at this line
-        // });
-        // var variableLength = deckNamesJSON.JSONobj.innerArray.length;
-        // for(var i=0; i<deckNamesJSON.JSONobj.innerArray.length; i++){
-        //     debugger;    //set to debug at this line
-        //     if(deckNamesJSON.JSONobj.innerArray[0][0][0][1]!=name && i==deckNamesJSON.JSONobj.innerArray.length){
-        //         debugger;    //set to debug at this line
-        //         deckNamesJSON.addToObj([[[0], [['deckName', name]]]]);
-        //         // variableLength++;
-        //     }else if(deckNamesJSON.JSONobj.innerArray[0][0][0][1]==name){
-        //         alreadyThereBool = true
-        //         debugger;    //set to debug at this line
-        //     }
-        // }
 
-        for(var i=0; i<deckNamesJSON.JSONobj.innerArray.length; i++){
-            debugger;    //set to debug at this line
-            if(deckNamesJSON.JSONobj.innerArray[i][0][0][1]==name){
-                alreadyThereBool = true;
-                debugger;    //set to debug at this line
-            }
-          }
-    }
     if(alreadyThereBool==true){
         console.log('Already Named!');
         alreadyThereBool=false;        
-        debugger;    //set to debug at this line
+        // debugger;    //set to debug at this line
     }else{
         console.log('NEW Deck Name');
         deckNamesJSON.addToObj([[[0], [['deckName', name]]]]);
-        debugger;    //set to debug at this line
+        // debugger;    //set to debug at this line
     }
-    debugger;    //set to debug at this line
+    // debugger;    //set to debug at this line
     saveDeckNames();
-    debugger;    //set to debug at this line
+    // debugger;    //set to debug at this line
 }
 
 function removeDeckName(name){
