382f4ee6ed6e0292fd9ac072650b6ab230fcd189
TheArborist <GOtrustD@verizon.net>
2022-04-15 22:19:35 -0700

    2022-04-15 - 10 minute Looping Commit

--- BIG_JSON.js
+++ BIG_JSON.js
@@ -1,8 +1,42 @@
 var theBIGJSON;
-var singleDeck;
-var cardData;
+var singleDeckJSON;
+var cardDataJSON;
 
 function initJSONVariables(){
     theBIGJSON = new JSON_Instance();
-    singleDeck = new JJ
+    singleDeckJSON = new JSON_Instance();
+    cardDataJSON = new JSON_Instance();
+}
+// =============================
+
+function uuidv4() {
+return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+});
+}
+
+
+function singleDeckJSONData(emailOwner, deckTitle, sharedWithEmails, deckScore){
+    sharedWithEmails == ""? sharedWithEmails = "": undefined;
+
+    singleDeckJSON.JSONobj.innerArray[0] = [[[0], [['emailOwner', emailOwner],['deckTitle', deckTitle], ['sharedWithEmails', sharedWithEmails], ['deckScore', deckScore], ['UID', uuidv4()]]]];
+}
+
+// =============================
+
+function makeCardDataJSON(question, answer){
+    cardDataJSON.JSONobj.innerArray[0] = [[[0], ['question', question], ['answer', answer]]];
+}
+
+
+
+
+// =============================
+function run(){
+    initJSONVariables();
+    makeCardDataJSON('time of day?', 'noon'); 
+    cardDataJSON.print();
+
+
 }
--- onload.js
+++ onload.js
@@ -2,40 +2,41 @@ window.onload = ()=>{
     hide();
     show('decks');
     
+    run();
     
     // addQuestionAddAnswerToPushFlashCards('', '');
     
     // myJSONFlashCards.saveToLocalStorage('saveMeFlash');
-    makeInstanceFlashCards();
-    initializeDeckNamesJSON();
-    // debugger;    //set to debug at this line
-    if(myJSONFlashCards.JSONobj == null || myJSONFlashCards.JSONobj.innerArray.length==0){
-        // debugger;    //set to debug at this line
-        firstIndex('karate');
-        addBlankBox(1)
-        $('.addBoxesHTMLHere').html(blankBoxHTML);
+    // makeInstanceFlashCards();
+    // initializeDeckNamesJSON();
+    // // debugger;    //set to debug at this line
+    // if(myJSONFlashCards.JSONobj == null || myJSONFlashCards.JSONobj.innerArray.length==0){
+    //     // debugger;    //set to debug at this line
+    //     firstIndex('karate');
+    //     addBlankBox(1)
+    //     $('.addBoxesHTMLHere').html(blankBoxHTML);
         
-        buttonListeners();
-        deleteButtons();
+    //     buttonListeners();
+    //     deleteButtons();
         
-        // addAnotherDeckToHTML(1, deckNamesJSON.JSONobj.innerArray[index][0][0][1], tags)
-        addAnotherDeckToHTML(1, "SCK", "nope")
+    //     // addAnotherDeckToHTML(1, deckNamesJSON.JSONobj.innerArray[index][0][0][1], tags)
+    //     addAnotherDeckToHTML(1, "SCK", "nope")
 
-        // runClick();
+    //     // runClick();
         
-        // debugger;    //set to debug at this line
-    }else{
-        // runClick();
-        retrieveIt();
-        runClick();
-        buttonListeners();
-        deleteButtons();
+    //     // debugger;    //set to debug at this line
+    // }else{
+    //     // runClick();
+    //     retrieveIt();
+    //     runClick();
+    //     buttonListeners();
+    //     deleteButtons();
         // debugger;    //set to debug at this line
         
         // retrieveIt();
         // populate();
         // debugger;    //set to debug at this line
-    }
+    // }
     // startupCSS();
 
 
